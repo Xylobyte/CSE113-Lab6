29a82d7cd5b77d8563664840d7d2d8419c5cc905
Xylobyte <dxgriego@gmail.com>
2020-10-22 16:59:56 -0600

    Finished difference

---
+++ difference.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#define MAX 100
+
+int main() {
+    int sumsq = 0;
+    int sqsum = 0;
+    for(int i = 1; i <= MAX; i++) {
+        sumsq += pow(i, 2);
+    }
+    for(int i = 1; i <= MAX; i++) {
+        sqsum += i;
+    }
+    sqsum = pow(sqsum, 2);
+    printf("The difference between the sum of the squares and the square of the sum from 1 to %d is %d\n", MAX, sqsum - sumsq);
+}
